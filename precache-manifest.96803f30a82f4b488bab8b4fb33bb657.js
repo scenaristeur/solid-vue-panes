c0a9fa95415f085d0538a21b1098f73066a2caec
David <scenaristeur@gmail.com>
2020-08-09 17:05:36 +0200

    save on editor

---
+++ precache-manifest.96803f30a82f4b488bab8b4fb33bb657.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8daca97248b9a9842fd",
+    "url": "/solid-vue-panes/css/about.330d107d.css"
+  },
+  {
+    "revision": "71bb5001c3239ce59337",
+    "url": "/solid-vue-panes/css/app.2293e1b0.css"
+  },
+  {
+    "revision": "c7369f57e7401662927e",
+    "url": "/solid-vue-panes/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "60a550e83a89ef83040e8ad660fade2b",
+    "url": "/solid-vue-panes/dist-popup/popup-template.html"
+  },
+  {
+    "revision": "7446e23a91934efa9156941824b3d35f",
+    "url": "/solid-vue-panes/dist-popup/popup.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/solid-vue-panes/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "befd1fcf7bb24248626907560da36103",
+    "url": "/solid-vue-panes/index.html"
+  },
+  {
+    "revision": "b8daca97248b9a9842fd",
+    "url": "/solid-vue-panes/js/about.52e6a612.js"
+  },
+  {
+    "revision": "71bb5001c3239ce59337",
+    "url": "/solid-vue-panes/js/app.0ceee4d1.js"
+  },
+  {
+    "revision": "c7369f57e7401662927e",
+    "url": "/solid-vue-panes/js/chunk-vendors.c9aa8753.js"
+  },
+  {
+    "revision": "110c8d460ca2b577b09e9aca01d7d161",
+    "url": "/solid-vue-panes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/solid-vue-panes/robots.txt"
+  },
+  {
+    "revision": "83dfc112ffad3b778c6f82c9147f94b8",
+    "url": "/solid-vue-panes/vendor/rdflib.min.js"
+  },
+  {
+    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
+    "url": "/solid-vue-panes/vendor/solid-file-client.bundle.js"
+  },
+  {
+    "revision": "942b236b1c9c4b0d70bf8fadc1f38310",
+    "url": "/solid-vue-panes/vendor/solid-query-ldflex.bundle.js"
+  },
+  {
+    "revision": "251b053187b51004112ec6c53a615781",
+    "url": "/solid-vue-panes/vendor/solid-query-ldflex.rdflib.js"
+  }
+]);
