2c330c0ed270e438fc236f3d6d513da6f9626fa6
Smag0 <scenaristeur@gmail.com>
2020-08-11 12:17:43 +0200

    design chat

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solid-vue-panes/precache-manifest.38ea2db93174c08faf584c88a036f080.js"
+  "/solid-vue-panes/precache-manifest.079d38ef5f38055afaf269a4c1afc824.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "solid-vue-panes"});
