f2684879fff42892854d386ea1dd66b3be09c74e
Smag0 <scenaristeur@gmail.com>
2020-08-15 15:30:46 +0200

    share target api test

---
+++ js/chunk-2d2257ad.51e0a708.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2257ad"],{e529:function(n,e,t){"use strict";t.r(e);var s=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},a=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"inbox"},[t("h1",[n._v("This is an inbox page")])])}],c={name:"Inbox",components:{}},i=c,o=t("2877"),l=Object(o["a"])(i,s,a,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d2257ad.51e0a708.js.map
