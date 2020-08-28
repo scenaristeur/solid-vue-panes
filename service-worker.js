1f61f030eca5416bbacd9c61d4880b56184b9aa0
Smag0 <scenaristeur@gmail.com>
2020-08-28 16:59:35 +0200

    autologin to storage.vue

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solid-vue-panes/precache-manifest.83244586dcd4919d115647547ed79047.js"
+  "/solid-vue-panes/precache-manifest.0e8700d1a32a2c28306b5e528a6b230c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "PoPock"});
