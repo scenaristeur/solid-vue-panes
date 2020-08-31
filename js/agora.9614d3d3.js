b8ab25dbc7214d8932b3db71b9380ce1eb90accd
David <scenaristeur@gmail.com>
2020-08-31 12:57:08 +0200

    agora notif

---
+++ js/agora.9614d3d3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["agora"],{de5b:function(n,e,a){"use strict";a.r(e);var t=function(){var n=this,e=n.$createElement,a=n._self._c||e;return a("div",{staticClass:"modele"},[a("Agora")],1)},o=[],r=(a("d3b7"),{name:"AgoraView",components:{Agora:function(){return a.e("chunk-2d0aba0d").then(a.bind(null,"15a4"))}}}),l=r,u=a("2877"),c=Object(u["a"])(l,t,o,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=agora.9614d3d3.js.map
